2beb47fd148cec0e93b497300a5841e63944492d
MekkaLabs <87154227+MekkaLabs@users.noreply.github.com>
2022-04-30 16:50:40 -0300

    Primeiro-Deploy
    Este deploy conta com as modificações basicas do Chainlist da DefiLhama para a versão da Real Valorizado.

---
+++ pages/index.js
@@ -0,0 +1,172 @@
+import React, { useMemo } from 'react';
+import Head from 'next/head';
+import styles from '../styles/Home.module.css';
+import { withTheme } from '@material-ui/core/styles';
+import { Typography, Button } from '@material-ui/core';
+import Chain from '../components/chain';
+import Header from '../components/header';
+import Image from 'next/image'
+import Link from 'next/link'
+
+import AddIcon from '@material-ui/icons/Add';
+import classes from './index.module.css';
+import { chainIds } from '../components/chains';
+import { fetcher } from '../utils/utils';
+import { useSearch, useTestnets } from '../stores';
+import allExtraRpcs from '../utils/extraRpcs.json';
+
+function removeEndingSlash(rpc) {
+  return rpc.endsWith('/') ? rpc.substr(0, rpc.length - 1) : rpc;
+}
+
+export async function getStaticProps({ params }) {
+  const chains = await fetcher('https://chainid.network/chains.json');
+  const chainTvls = await fetcher('https://api.llama.fi/chains');
+
+  function populateChain(chain) {
+    const extraRpcs = allExtraRpcs[chain.chainId]?.rpcs;
+    if (extraRpcs !== undefined) {
+      const rpcs = new Set(chain.rpc.map(removeEndingSlash).filter((rpc) => !rpc.includes('${INFURA_API_KEY}')));
+      extraRpcs.forEach((rpc) => rpcs.add(removeEndingSlash(rpc)));
+      chain.rpc = Array.from(rpcs);
+    }
+    const chainSlug = chainIds[chain.chainId];
+    if (chainSlug !== undefined) {
+      const defiChain = chainTvls.find((c) => c.name.toLowerCase() === chainSlug);
+      return defiChain === undefined
+        ? chain
+        : {
+          ...chain,
+          tvl: defiChain.tvl,
+          chainSlug,
+        };
+    }
+    return chain;
+  }
+
+  const sortedChains = chains
+    .filter((c) => c.name !== '420coin') // same chainId as ronin
+    .map(populateChain)
+    .sort((a, b) => {
+      return (b.tvl ?? 0) - (a.tvl ?? 0);
+    });
+
+  return {
+    props: {
+      sortedChains,
+    },
+    revalidate: 3600,
+  };
+}
+
+function Home({ changeTheme, theme, sortedChains }) {
+  const testnets = useTestnets((state) => state.testnets);
+  const search = useSearch((state) => state.search);
+
+  const addNetwork = () => {
+    window.open('https://github.com/ethereum-lists/chains', '_blank');
+  };
+
+  const addRpc = () => {
+    window.open('https://github.com/DefiLlama/chainlist/blob/main/utils/extraRpcs.json', '_blank');
+  };
+
+  const chains = useMemo(() => {
+    if (!testnets) {
+      return sortedChains.filter((item) => {
+        const testnet =
+          item.name?.toLowerCase().includes('test') ||
+          item.title?.toLowerCase().includes('test') ||
+          item.network?.toLowerCase().includes('test');
+        return !testnet;
+      });
+    } else return sortedChains;
+  }, [testnets, sortedChains]);
+
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Chainlist - Real Valorizado</title>
+        <link rel="icon" href="/favicon.png" />
+      </Head>
+
+      <main className={styles.main}>
+        <div className={theme.palette.type === 'dark' ? classes.containerDark : classes.container}>
+
+          <div className={classes.copyContainer}>
+
+            <div className={classes.copyCentered}>
+              <div className={classes.logo}>
+                <Link href="https://realvalorizado.com.br">
+                  <a>
+                    <Image
+                      src="/real-valorizado-logo-blue.png"
+                      alt="Logo Real Valorizado"
+                      width="232"
+                      height="75"
+                    />
+                  </a>
+                </Link>
+              </div>
+
+
+
+              <Typography variant="h1" className={classes.chainListSpacing}>
+                <span className={classes.helpingUnderline}>Chainlist</span>
+              </Typography>
+              <Typography variant="h2" className={classes.helpingParagraph}>
+                Ajudando os usuários a se conectarem ao poder das redes Blockchain EVM
+              </Typography>
+              <Typography className={classes.subTitle}>
+                Chainlist é uma lista de redes EVM (Virtual Machines do Ethereum). Os usuários podem usar os links para conectar suas carteiras Web3 como a Metamask a aplicações e Dapps com ID para cada rede.
+              </Typography>
+
+              <div className={classes.socials}>
+                <a
+                  className={`${classes.socialButton}`}
+                  href="https://t.me/+VTfvJk5zhfI3Mzgx"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <svg version="1.1" width="24" height="24" viewBox="0 0 24 24">
+                    <path
+                      fill={'#0528f2'}
+                      d="M18.384,22.779c0.322,0.228 0.737,0.285 1.107,0.145c0.37,-0.141 0.642,-0.457 0.724,-0.84c0.869,-4.084 2.977,-14.421 3.768,-18.136c0.06,-0.28 -0.04,-0.571 -0.26,-0.758c-0.22,-0.187 -0.525,-0.241 -0.797,-0.14c-4.193,1.552 -17.106,6.397 -22.384,8.35c-0.335,0.124 -0.553,0.446 -0.542,0.799c0.012,0.354 0.25,0.661 0.593,0.764c2.367,0.708 5.474,1.693 5.474,1.693c0,0 1.452,4.385 2.209,6.615c0.095,0.28 0.314,0.5 0.603,0.576c0.288,0.075 0.596,-0.004 0.811,-0.207c1.216,-1.148 3.096,-2.923 3.096,-2.923c0,0 3.572,2.619 5.598,4.062Zm-11.01,-8.677l1.679,5.538l0.373,-3.507c0,0 6.487,-5.851 10.185,-9.186c0.108,-0.098 0.123,-0.262 0.033,-0.377c-0.089,-0.115 -0.253,-0.142 -0.376,-0.064c-4.286,2.737 -11.894,7.596 -11.894,7.596Z"
+                    />
+                  </svg>
+                  <Typography variant="body1" className={classes.sourceCode}>
+                    Grupo no telegram
+                  </Typography>
+                </a>
+
+              </div>
+            </div>
+          </div>
+          <div className={theme.palette.type === 'dark' ? classes.listContainerDark : classes.listContainer}>
+            <Header changeTheme={changeTheme} />
+            <div className={classes.cardsContainer}>
+              {(search === ''
+                ? chains
+                : chains.filter((chain) => {
+                  //filter
+                  return (
+                    chain.chain.toLowerCase().includes(search.toLowerCase()) ||
+                    chain.chainId.toString().toLowerCase().includes(search.toLowerCase()) ||
+                    chain.name.toLowerCase().includes(search.toLowerCase()) ||
+                    (chain.nativeCurrency ? chain.nativeCurrency.symbol : '')
+                      .toLowerCase()
+                      .includes(search.toLowerCase())
+                  );
+                })
+              ).map((chain, idx) => {
+                return <Chain chain={chain} key={idx} />;
+              })}
+            </div>
+          </div>
+        </div>
+      </main>
+    </div>
+  );
+}
+
+export default withTheme(Home);
