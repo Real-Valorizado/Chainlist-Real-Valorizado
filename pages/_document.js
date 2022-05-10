7cc172b1dff9c43545d3eb9eed92d5cd94b86c6e
MekkaLabs <87154227+MekkaLabs@users.noreply.github.com>
2022-05-10 15:20:43 -0300

    trackeamento

--- pages/_document.js
+++ pages/_document.js
@@ -11,10 +11,43 @@ export default class MyDocument extends Document {
         <Head>
           <link rel="stylesheet" href="/fonts/Inter/Inter.css" />
           <link rel="stylesheet" href="/fonts/Druk/Druk.css" />
+          <script dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+                            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+                            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+                            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+                            })(window,document,'script','dataLayer','GTM-PFLF854');`}}>
+          </script>
+
+          <meta name="facebook-domain-verification" content="ydzlofoh8b0wqbif3bvg12ptw8757x" />
+
+          <script dangerouslySetInnerHTML={{
+            __html: `
+                    !function(f,b,e,v,n,t,s)
+                    {if(f.fbq)return;n=f.fbq=function(){n.callMethod ?
+                        n.callMethod.apply(n, arguments) : n.queue.push(arguments)};
+                    if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+                    n.queue=[];t=b.createElement(e);t.async=!0;
+                    t.src=v;s=b.getElementsByTagName(e)[0];
+s.parentNode.insertBefore(t,s)}(window, document,'script',
+                    'https://connect.facebook.net/en_US/fbevents.js');
+                    fbq('init', '216372647319656');
+                    fbq('track', 'PageView');`}}>
+          </script>
+
+          <noscript dangerouslySetInnerHTML={{
+            __html: `<img height="1" width="1" style="display:none"
+                    src="https://www.facebook.com/tr?id=216372647319656&ev=PageView&noscript=1"
+                />`}}>
+          </noscript>
         </Head>
         <body>
 
           <Main />
+          <noscript dangerouslySetInnerHTML={{
+            __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-PFLF854"
+height="0" width="0" style="display:none;visibility:hidden"></iframe>`}}>
+          </noscript>
           <NextScript />
         </body>
       </Html>
